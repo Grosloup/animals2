24db38b328e17c899c5046643cee0e72114fb4f3
Grosloup <canfrere.nicolas@orange.fr>
2013-06-06 00:07:55 +0200

    --

--- src/Site/AdminBundle/Entity/EventType.php
+++ src/Site/AdminBundle/Entity/EventType.php
@@ -3,6 +3,7 @@
 namespace Site\AdminBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * EventType
@@ -27,6 +28,18 @@ class EventType
      * @ORM\Column(name="name", type="string", length=255)
      */
     private $name;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Site\AdminBundle\Entity\Color")
+     * @ORM\JoinColumn(name="color_id", referencedColumnName="id", onDelete="SET NULL")
+     */
+    protected $color;
+
+    /**
+     * @ORM\Column(name="slug", type="string", length=255)
+     * @Gedmo\Slug(fields={"name"})
+     */
+    protected $slug;
 
 
     /**
@@ -60,5 +73,51 @@ class EventType
     public function getName()
     {
         return $this->name;
+    }
+
+    /**
+     * Set color
+     *
+     * @param Color $color
+     * @return EventType
+     */
+    public function setColor(Color $color = null)
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    /**
+     * Get color
+     *
+     * @return Color
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     * @return EventType
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+    
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string 
+     */
+    public function getSlug()
+    {
+        return $this->slug;
     }
 }
