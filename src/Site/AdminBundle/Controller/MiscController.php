0e30fcee382204a6f100a8d8f780e3f7d735a57c
Grosloup <canfrere.nicolas@orange.fr>
2013-06-05 13:21:13 +0200

    --

---
+++ src/Site/AdminBundle/Controller/MiscController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Date: 05/06/13
+ * Time: 13:17
+ * @author Nicolas Canfrere <nico.canfrere at gmail.com>
+ */
+
+namespace Site\AdminBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MiscController extends Controller
+{
+    public function indexAction()
+    {
+
+    }
+}
