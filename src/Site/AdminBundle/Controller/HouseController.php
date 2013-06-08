e3dcec3e3892b76e4f7c9e7e410e82075caa7c99
Grosloup <canfrere.nicolas@orange.fr>
2013-06-08 13:48:50 +0200

    --

--- src/Site/AdminBundle/Controller/HouseController.php
+++ src/Site/AdminBundle/Controller/HouseController.php
@@ -7,6 +7,8 @@
 
 namespace Site\AdminBundle\Controller;
 
+use Site\AdminBundle\Entity\House;
+use Site\AdminBundle\Form\HouseType;
 use Symfony\Component\HttpFoundation\Request;
 
 class HouseController extends BaseController implements CRUDInterface
@@ -14,36 +16,97 @@ class HouseController extends BaseController implements CRUDInterface
 
     public function indexAction()
     {
-        // TODO: Implement indexAction() method.
+        $entities = $this->getRepo("AdminBundle:House")->findAll();
+        return $this->render("AdminBundle:Misc:House/index.html.twig",["entities"=>$entities]);
     }
 
     public function showAction($id)
     {
-        // TODO: Implement showAction() method.
+        $entity = $this->getRepo("AdminBundle:House")->find($id);
+        if (!$entity) {
+            throw $this->createNotFoundException('Bâtiment introuvable.');
+        }
+        return $this->render("AdminBundle:Misc:House/show.html.twig",["entity"=>$entity]);
     }
 
     public function newAction()
     {
-        // TODO: Implement newAction() method.
+        $entity = new House();
+        $form = $this->createForm(new HouseType(), $entity);
+        return $this->render("AdminBundle:Misc:House/new.html.twig",["form"=>$form->createView()]);
     }
 
     public function createAction(Request $request)
     {
-        // TODO: Implement createAction() method.
+        $entity = new House();
+        $form = $this->createForm(new HouseType(), $entity);
+        if($request->isMethod("POST")){
+            $form->submit($request);
+            if($form->isValid()){
+                $em = $this->getEm();
+                $em->persist($entity);
+                $em->flush();
+                return $this->redirect($this->generateUrl("admin_misc_homepage"));
+            }
+        }
+        return $this->render("AdminBundle:Misc:House/new.html.twig", ["form"=>$form->createView()]);
     }
 
     public function editAction($id)
     {
-        // TODO: Implement editAction() method.
+        $entity = $this->getRepo("AdminBundle:House")->find($id);
+        if (!$entity) {
+            throw $this->createNotFoundException('Bâtiment introuvable.');
+        }
+        $form = $this->createForm(new HouseType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+        return $this->render("AdminBundle:Misc:House/edit.html.twig", ["form"=>$form->createView(),"delete_form"=>$deleteForm->createView(), "entity"=>$entity]);
     }
 
     public function updateAction($id, Request $request)
     {
-        // TODO: Implement updateAction() method.
+        $em = $this->getEm();
+        $entity = $em->getRepository("AdminBundle:House")->find($id);
+        if(!$entity){
+            throw $this->createNotFoundException("Bâtiment introuvable");
+        }
+        $form = $this->createForm(new HouseType(), $entity);
+        $form->submit($request);
+        if($form->isValid()){
+            $em = $this->getEm();
+            $em->persist($entity);
+            $em->flush();
+            return $this->redirect($this->generateUrl("admin_misc_homepage"));
+        }
+        $deleteForm = $this->createDeleteForm($id);
+        return $this->render("AdminBundle:Misc:House/edit.html.twig", ["form"=>$form->createView(),"delete_form"=>$deleteForm->createView(), "entity"=>$entity]);
     }
 
     public function deleteAction($id, Request $request)
     {
-        // TODO: Implement deleteAction() method.
+        $form = $this->createDeleteForm($id);
+        $form->submit($request);
+
+        if ($form->isValid()) {
+            $em = $this->getEm();
+            $entity = $em->getRepository('AdminBundle:House')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Bâtiment introuvable.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('admin_misc_homepage'));
+    }
+
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->getForm()
+            ;
     }
 }
---
+++ src/Site/AdminBundle/Form/HouseType.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Date: 08/06/13
+ * Time: 13:37
+ * @author Nicolas Canfrere <nico.canfrere at gmail.com>
+ */
+
+namespace Site\AdminBundle\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class HouseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add("name");
+    }
+
+    public function finishView(FormView $view, FormInterface $form, array $options)
+    {
+        $view["name"]->vars["label"] = "Nom";
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Site\AdminBundle\Entity\House'
+        ));
+    }
+
+
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        return "house_form";
+    }
+}
