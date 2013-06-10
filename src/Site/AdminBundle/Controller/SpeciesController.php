5169d1e8f83288071f4a8f9f3c935cb3173c286c
Grosloup <canfrere.nicolas@orange.fr>
2013-06-10 07:54:17 +0200

    --

---
+++ src/Site/AdminBundle/Controller/SpeciesController.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Date: 10/06/13
+ * Time: 07:38
+ * @author Nicolas Canfrere <nico.canfrere at gmail.com>
+ */
+
+namespace Site\AdminBundle\Controller;
+
+
+use Symfony\Component\HttpFoundation\Request;
+
+class SpeciesController extends BaseController implements CRUDInterface
+{
+
+    public function indexAction()
+    {
+        // TODO: Implement indexAction() method.
+    }
+
+    public function showAction($id)
+    {
+        // TODO: Implement showAction() method.
+    }
+
+    public function newAction()
+    {
+        // TODO: Implement newAction() method.
+    }
+
+    public function createAction(Request $request)
+    {
+        // TODO: Implement createAction() method.
+    }
+
+    public function editAction($id)
+    {
+        // TODO: Implement editAction() method.
+    }
+
+    public function updateAction($id, Request $request)
+    {
+        // TODO: Implement updateAction() method.
+    }
+
+    public function deleteAction($id, Request $request)
+    {
+        // TODO: Implement deleteAction() method.
+    }
+}
