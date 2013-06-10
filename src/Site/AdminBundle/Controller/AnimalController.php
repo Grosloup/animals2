f9352813090a59e3af41cae4164c863ecd340c3c
Grosloup <canfrere.nicolas@orange.fr>
2013-06-10 08:46:11 +0200

    --

--- src/Site/AdminBundle/Controller/AnimalController.php
+++ src/Site/AdminBundle/Controller/AnimalController.php
@@ -8,6 +8,8 @@
 namespace Site\AdminBundle\Controller;
 
 
+use Site\AdminBundle\Entity\Animal;
+use Site\AdminBundle\Form\AnimalType;
 use Symfony\Component\HttpFoundation\Request;
 
 class AnimalController extends BaseController implements CRUDInterface
@@ -15,36 +17,92 @@ class AnimalController extends BaseController implements CRUDInterface
 
     public function indexAction()
     {
-        // TODO: Implement indexAction() method.
+        $entities = $this->getRepo("AdminBundle:Animal")->findAll();
+        return $this->render("AdminBundle:Animal:index.html.twig",["entities"=>$entities]);
     }
 
     public function showAction($id)
     {
-        // TODO: Implement showAction() method.
+        $entity = $this->findEntity($id);
+        return $this->render("AdminBundle:Animal:show.html.twig",["entity"=>$entity]);
     }
 
     public function newAction()
     {
-        // TODO: Implement newAction() method.
+        $entity = new Animal();
+        $form = $this->createForm(new AnimalType(), $entity);
+        return $this->render("AdminBundle:Animal:new.html.twig",["form"=>$form->createView()]);
     }
 
     public function createAction(Request $request)
     {
-        // TODO: Implement createAction() method.
+        $entity = new Animal();
+        $form = $this->createForm(new AnimalType(), $entity);
+        if($request->isMethod("POST")){
+            $form->submit($request);
+            if($form->isValid()){
+                $em = $this->getEm();
+                $em->persist($entity);
+                $em->flush();
+                return $this->redirect($this->generateUrl("admin_animal_index"));
+            }
+        }
+        return $this->render("AdminBundle:Animal:new.html.twig", ["form"=>$form->createView()]);
     }
 
     public function editAction($id)
     {
-        // TODO: Implement editAction() method.
+        $entity = $this->findEntity($id);
+        $form = $this->createForm(new AnimalType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+        return $this->render("AdminBundle:Animal:edit.html.twig", ["form"=>$form->createView(),"delete_form"=>$deleteForm->createView(), "entity"=>$entity]);
     }
 
     public function updateAction($id, Request $request)
     {
-        // TODO: Implement updateAction() method.
+        $entity = $this->findEntity($id);
+        $form = $this->createForm(new AnimalType(), $entity);
+        $form->submit($request);
+        if($form->isValid()){
+            $em = $this->getEm();
+            $em->persist($entity);
+            $em->flush();
+            return $this->redirect($this->generateUrl("admin_animal_index"));
+        }
+        $deleteForm = $this->createDeleteForm($id);
+        return $this->render("AdminBundle:Animal:edit.html.twig", ["form"=>$form->createView(),"delete_form"=>$deleteForm->createView(), "entity"=>$entity]);
     }
 
     public function deleteAction($id, Request $request)
     {
-        // TODO: Implement deleteAction() method.
+        $form = $this->createDeleteForm($id);
+        $form->submit($request);
+
+        if ($form->isValid()) {
+            $em = $this->getEm();
+            $entity = $this->findEntity($id);
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('admin_animal_index'));
+    }
+
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->getForm()
+            ;
+    }
+
+    private function findEntity($id)
+    {
+        $entity = $this->getRepo("AdminBundle:Animal")->find($id);
+        if (!$entity) {
+            throw $this->createNotFoundException('Animal ' . $id . ' introuvable.');
+        }
+        return $entity;
     }
 }
---
+++ src/Site/AdminBundle/Form/AnimalType.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Date: 10/06/13
+ * Time: 08:26
+ * @author Nicolas Canfrere <nico.canfrere at gmail.com>
+ */
+
+namespace Site\AdminBundle\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class AnimalType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Site\AdminBundle\Entity\Animal'
+        ));
+    }
+
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        return "animal_form";
+    }
+
+    public function finishView(FormView $view, FormInterface $form, array $options)
+    {
+        $view["name"]->vars["label"] = "Nom";
+
+    }
+}
